4a19cfb142575e90e486bbb65b0e61148792f3aa
IMRANDIL <aliimranadil2@gmail.com>
2022-03-15 16:30:00 +0530

    yh

--- models/user.js
+++ models/user.js
@@ -30,6 +30,55 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 
+//virtual field...
+
+userSchema.virtual('password').set((password) => {
+
+    //create temporary variable....called _password...
+
+    this._password = password;
+
+    //generate a timestamp, uuidv1 gives us the unix timestamp;
+
+    this.salt = uuidv1();
+
+
+    //encrypt the password function call...
+
+    this.hashedPassword = this.encryptPassword(password)
+
+
+
+});
+
+
+
+//methods......
+
+userSchema.methods = {
+    encryptPassword: function (password) {
+        if (!password) return '';
+
+        try {
+
+            return crypto.createHmac('sha256', this.salt).update(password).digest('hex')
+
+        } catch (error) {
+            return '';
+        }
+
+
+
+    },
+    authenticate: function (plainTxt) {
+        return this.encryptPassword(plainTxt) === this.hashedPassword;
+    }
+}
+
+
+
+
+
 
 module.exports = mongoose.model('User', userSchema);
 
